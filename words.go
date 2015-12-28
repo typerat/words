f06710a229cbbaf13ab430afb76aa39f7fe073d4
Christoph Mudri <christoph1703@gmail.com>
2015-12-28 02:06:52 +0100

    add comments

--- words.go
+++ words.go
@@ -6,21 +6,32 @@ import (
 	"time"
 )
 
+// get pseudorandom words for given byte slice
 func Get(input []byte) string {
+	// hash input data
 	hash := sha256.Sum256(input)
 
+	// fold hash to int64
 	var seed int64
 	for i, e := range hash {
 		seed += int64(e) << ((uint(i) % 8) * 8)
 	}
+
+	// set seed for pseudorandom generator
 	rand.Seed(seed)
+
+	// return pseudorandom words
 	return Adjectives[rand.Intn(len(Adjectives))] + " " + Animals[rand.Intn(len(Animals))]
 }
 
+// get almost true random words
 func Random() string {
+	// get some randomness
 	b := time.Now().UnixNano()
 
+	// seed the pseudorandom generator
 	rand.Seed(b)
 
+	// return random words
 	return Adjectives[rand.Intn(len(Adjectives))] + " " + Animals[rand.Intn(len(Animals))]
 }
