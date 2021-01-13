e98d0ef7bb5a71c0b3ecc923af41782fd3d0161f
Christoph Armster <christoph1703@gmail.com>
2021-01-13 23:25:45 +0100

    add some (quite useless) tests

---
+++ words_test.go
@@ -0,0 +1,33 @@
+package words
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func ExampleGet() {
+	words := Get([]byte("pseudorandom"))
+	fmt.Println(words)
+	// Output: idiotic goose
+}
+
+// random tests are quite useless, but it feels good to have 100% coverage
+
+func TestRandom(t *testing.T) {
+	words := Random()
+
+	parts := strings.Split(words, " ")
+
+	if len(parts) != 2 {
+		t.Error("expected two words")
+	}
+}
+
+func TestRandomAdjective(t *testing.T) {
+	word := RandomAdjective()
+
+	if len(word) == 0 {
+		t.Error("expected a word")
+	}
+}
